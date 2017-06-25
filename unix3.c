7cbb5b3b6078e7fb291abfc0bc18ef66584bde32
wangmenghu <wangmenghu@jd.com>
2017-06-25 22:01:40 +0800

    10.20

--- unix3.c
+++ unix3.c
@@ -13,37 +13,72 @@ void err_sys(const char* string)
 {
 	printf(string);
 }
+#include <errno.h>
+void
+pr_mask(const char *str)
+{
+	sigset_t sigset;
+	int errno_save;
+	errno_save = errno; /* we can be called by signal handlers */
+	if (sigprocmask(SIG_BLOCK, NULL, &sigset) < 0) {
+		err_sys("sigprocmask error");
+	}
+	else {
+		printf("%s", str);
+		if (sigismember(&sigset, SIGINT))
+			printf(" SIGINT");
+		if (sigismember(&sigset, SIGQUIT))
+			printf(" SIGQUIT");
+		if (sigismember(&sigset, SIGUSR1))
+			printf(" SIGUSR1");
+		if (sigismember(&sigset, SIGALRM))
+			printf(" SIGALRM");
+		/* remaining signals can go here */
+		printf("\n");
+	}
+	errno = errno_save; /* restore errno */
+}
+#include <setjmp.h>
+#include <time.h>
+static void sig_usr1(int);
+static void sig_alrm(int);
+static sigjmp_buf jmpbuf;
+static volatile sig_atomic_t canjump;
 int
 main(void)
 {
-	sigset_t newmask, oldmask, pendmask;
-	if (signal(SIGQUIT, sig_quit) == SIG_ERR)
-		err_sys("can¡¯t catch SIGQUIT");
-	/*
-	* Block SIGQUIT and save current signal mask.
-	*/
-	sigemptyset(&newmask);
-	sigaddset(&newmask, SIGQUIT);
-	if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
-		err_sys("SIG_BLOCK error");
-	sleep(5); /* SIGQUIT here will remain pending */
-	if (sigpending(&pendmask) < 0)
-		err_sys("sigpending error");
-	if (sigismember(&pendmask, SIGQUIT))
-		printf("\nSIGQUIT pending\n");
-	/*
-	* Restore signal mask which unblocks SIGQUIT.
-	*/
-	if (sigprocmask(SIG_UNBLOCK, &newmask, NULL) < 0)
-		err_sys("SIG_SETMASK error");
-	printf("SIGQUIT unblocked\n");
-	sleep(5); /* SIGQUIT here will terminate with core file */
-	exit(0);
+	if (signal(SIGUSR1, sig_usr1) == SIG_ERR)
+		err_sys("signal(SIGUSR1) error");
+	if (signal(SIGALRM, sig_alrm) == SIG_ERR)
+		err_sys("signal(SIGALRM) error");
+	pr_mask("starting main: "); /* Figure 10.14 */
+	printf("the proccess id is %d\n", getpid());
+	if (sigsetjmp(jmpbuf, 1)) {
+		pr_mask("ending main: ");
+		exit(0);
+	}
+	canjump = 1; /* now sigsetjmp() is OK */
+	for (;;)
+		pause();
 }
 static void
-sig_quit(int signo)
+sig_usr1(int signo)
 {
-	printf("caught SIGQUIT\n");
-	if (signal(SIGQUIT, SIG_DFL) == SIG_ERR)
-		err_sys("can¡¯t reset SIGQUIT");
+	time_t starttime;
+	if (canjump == 0)
+		return; /* unexpected signal, ignore */
+	pr_mask("starting sig_usr1: ");
+	alarm(3); /* SIGALRM in 3 seconds */
+	starttime = time(NULL);
+	for (;;) /* busy wait for 5 seconds */
+	if (time(NULL) > starttime + 5)
+		break;
+	pr_mask("finishing sig_usr1: ");
+	canjump = 0;
+	siglongjmp(jmpbuf, 1); /* jump back to main, don¡¯t return */
+}
+static void
+sig_alrm(int signo)
+{
+	pr_mask("in sig_alrm: ");
 }
