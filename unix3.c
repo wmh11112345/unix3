ea2bd87086b2fe1ea2404762f888f55bc0e6c3fb
wangmenghu <wangmenghu@jd.com>
2017-07-05 14:44:13 +0800

    10.24

--- unix3.c
+++ unix3.c
@@ -29,52 +29,47 @@ sig_chld(int signo)
 #include <errno.h>
 #include <signal.h>
 #include <unistd.h>
-int
-system(const char *cmdstring) /* with appropriate signal handling */
+ssize_t /* Read "n" bytes from a descriptor */
+readn(int fd, void *ptr, size_t n)
 {
-	pid_t pid;
-	int status;
-	struct sigaction ignore, saveintr, savequit;
-	sigset_t chldmask, savemask;
-	if (cmdstring == NULL)
-		return(1); /* always a command processor with UNIX */
-	ignore.sa_handler = SIG_IGN; /* ignore SIGINT and SIGQUIT */
-	sigemptyset(&ignore.sa_mask);
-	ignore.sa_flags = 0;
-	if (sigaction(SIGINT, &ignore, &saveintr) < 0)
-		return(-1);
-	if (sigaction(SIGQUIT, &ignore, &savequit) < 0)
-		return(-1);
-	sigemptyset(&chldmask); /* now block SIGCHLD */
-	sigaddset(&chldmask, SIGCHLD);
-	if (sigprocmask(SIG_BLOCK, &chldmask, &savemask) < 0)
-		return(-1);
-	if ((pid = fork()) < 0) {
-		status = -1; /* probably out of processes */
+	size_t nleft;
+	ssize_t nread;
+	nleft = n;
+	while (nleft > 0) {
+		if ((nread = read(fd, ptr, nleft)) < 0) {
+			if (nleft == n)
+				return(-1); /* error, return -1 */
+			else
+				break; /* error, return amount read so far */
+		}
+		else if (nread == 0) {
+			break; /* EOF */
+		}
+		nleft -= nread;
+		ptr += nread;
 	}
-	else if (pid == 0) { /* child */
-		/* restore previous signal actions & reset signal mask */
-		sigaction(SIGINT, &saveintr, NULL);
-		sigaction(SIGQUIT, &savequit, NULL);
-		sigprocmask(SIG_SETMASK, &savemask, NULL);
-		execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);
-		_exit(127); /* exec error */
-	}
-	else { /* parent */
-		while (waitpid(pid, &status, 0) < 0)
-		if (errno != EINTR) {
-			status = -1; /* error other than EINTR from waitpid() */
+	return(n - nleft); /* return >= 0 */
+}
+ssize_t /* Write "n" bytes to a descriptor */
+writen(int fd, const void *ptr, size_t n)
+{
+	size_t nleft;
+	ssize_t nwritten;
+	nleft = n;
+	while (nleft > 0) {
+		if ((nwritten = write(fd, ptr, nleft)) < 0) {
+			if (nleft == n)
+				return(-1); /* error, return -1 */
+			else
+				break; /* error, return amount written so far */
+		}
+		else if (nwritten == 0) {
 			break;
 		}
+		nleft -= nwritten;
+		ptr += nwritten;
 	}
-	/* restore previous signal actions & reset signal mask */
-	if (sigaction(SIGINT, &saveintr, NULL) < 0)
-		return(-1);
-	if (sigaction(SIGQUIT, &savequit, NULL) < 0)
-		return(-1);
-	if (sigprocmask(SIG_SETMASK, &savemask, NULL) < 0)
-		return(-1);
-	return(status);
+	return(n - nleft); /* return >= 0 */
 }
 
 int
