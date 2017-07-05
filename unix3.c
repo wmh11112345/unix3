c4a7db705feea0c02276615d92f678d1d582b26d
wangmenghu <wangmenghu@jd.com>
2017-07-05 18:05:22 +0800

    18.10 Disable interrupt character and change end-of-file character

--- unix3.c
+++ unix3.c
@@ -9,49 +9,27 @@
 
 static jmp_buf env_alrm;
 static void sig_quit(int);
-void err_sys(const char* string)
-{
-	printf(string);
-}
+#define err_sys perror
+#define err_quit perror
 
-#include <fcntl.h>
-#include <sys/mman.h>
-#define COPYINCR (1024*1024*1024) /* 1 GB */
+#include <termios.h>
 int
-main(int argc, char *argv[])
+main(void)
 {
-	int fdin, fdout;
-	void *src, *dst;
-	size_t copysz;
-	struct stat sbuf;
-	off_t fsz = 0;
-	if (argc != 3)
-		err_sys("usage: %s <fromfile> <tofile>");
-	if ((fdin = open(argv[1], O_RDONLY)) < 0)
-		err_sys("can¡¯t open %s for reading");
-	if ((fdout = open(argv[2], O_RDWR | O_CREAT | O_TRUNC,
-		S_IWUSR|S_IWOTH|S_IWGRP)) < 0)
-		err_sys("can¡¯t creat %s for writing");
-	if (fstat(fdin, &sbuf) < 0) /* need size of input file */
-		err_sys("fstat error");
-	if (ftruncate(fdout, sbuf.st_size) < 0) /* set output file size */
-		err_sys("ftruncate error");
-	while (fsz < sbuf.st_size) {
-		if ((sbuf.st_size - fsz) > COPYINCR)
-			copysz = COPYINCR;
-		else
-			copysz = sbuf.st_size - fsz;
-		if ((src = mmap(0, copysz, PROT_READ, MAP_SHARED,
-			fdin, fsz)) == MAP_FAILED)
-			err_sys("mmap error for input");
-		if ((dst = mmap(0, copysz, PROT_READ | PROT_WRITE,
-			MAP_SHARED, fdout, fsz)) == MAP_FAILED)
-			err_sys("mmap error for output");
-		memcpy(dst, src, copysz); /* does the file copy */
-		msync(dst, copysz, MS_SYNC);
-		munmap(src, copysz);
-		munmap(dst, copysz);
-		fsz += copysz;
-	}
+	struct termios term;
+	long vdisable;
+	char a[10];
+	if (isatty(STDIN_FILENO) == 0)
+		err_quit("standard input is not a terminal device");
+	if ((vdisable = fpathconf(STDIN_FILENO, _PC_VDISABLE)) < 0)
+		err_quit("fpathconf error or _POSIX_VDISABLE not in effect");
+	if (tcgetattr(STDIN_FILENO, &term) < 0) /* fetch tty state */
+		err_sys("tcgetattr error");
+	scanf("%s", a);
+	term.c_cc[VINTR] = vdisable; /* disable INTR character */
+	term.c_cc[VEOF] = 2; /* EOF is Control-B */
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &term) < 0)
+		err_sys("tcsetattr error");
+	scanf("%s", a);
 	exit(0);
 }
